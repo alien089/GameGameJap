48b5d5c747e9e6ff1405bf85c00a9274c51f2496
arthley <arthleyfamorca@gmail.com>
2023-02-04 19:56:49 +0100

    Player Animation & Player Movement update

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,7 +9,11 @@ public class PlayerController : MonoBehaviour
 {
     [Range (0,100)] public float Speed; //Movement Speed of the pg
     [Range(0, 100)] public float JumpHeight; //How high is the jump 
+    
     private Rigidbody2D m_Body;
+    private Animator m_Animator;
+    private SpriteRenderer m_SR;
+
     public bool m_Grounded; //To see if the player is on the ground
     private float m_HorizontalInput;
 
@@ -29,20 +33,26 @@ public class PlayerController : MonoBehaviour
     {
         cam = Camera.main;
         mousePos = new Vector2();
+        m_Animator = GetComponent<Animator>();
+        m_SR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (m_Grounded) m_Animator.SetBool("IsJumping", false);
         if (canMove)
         {
             m_HorizontalInput = Input.GetAxis("Horizontal");    //When you press leftKeys -1, rightKeys +1
-            m_Body.velocity = new Vector2(m_HorizontalInput * Speed, m_Body.velocity.y);  //Movement of the pg
+            m_Body.velocity = new Vector2(m_HorizontalInput * Speed, m_Body.velocity.y);
+            if(m_Body.velocity.magnitude > 0) m_Animator.SetBool("IsWalking", true);
+            else m_Animator.SetBool("IsWalking", false);  //Movement of the pg
 
-            if (m_HorizontalInput < 0.001f)    //if he is facing left turn/flip left
-                transform.localScale = new Vector3(-1, 1, 1);
-            else if (m_HorizontalInput > 0.001f)  //else turn/flip right
-                transform.localScale = new Vector3(1, 1, 1);
+            if (Input.GetKey(KeyCode.LeftShift)) m_Animator.SetBool("IsRunning", true);
+            else m_Animator.SetBool("IsRunning", false);
+
+            if(m_Body.velocity.x < 0) m_SR.flipX = true;
+            else if (m_Body.velocity.x > 0) m_SR.flipX = false;
 
             if (Input.GetKey(KeyCode.Space) && m_Grounded)    //If Space Key is pressed and the pg is not on the ground the pg is going to jump
                 Jump(); //Calling Jump method
@@ -53,7 +63,8 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        m_Body.velocity = new Vector2(m_Body.velocity.x, JumpHeight); //Move the pg above / jump
+        m_Body.velocity = new Vector2(m_Body.velocity.x, JumpHeight);
+        m_Animator.SetBool("IsJumping", true); //Move the pg above / jump
         m_Grounded = false; //We are not touching the ground so its false
     }
 
